625e51d109a32284be37fe6deb7192443cb13e2e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-05-24 18:58:12 +0800

    updated readme

--- src/Page/App.tsx
+++ src/Page/App.tsx
@@ -7,12 +7,12 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
 
 
 export const App: React.FC<IProps> = (props) => {
   return (
     <TableContainer component={Paper}>
-
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -23,31 +23,30 @@ export const App: React.FC<IProps> = (props) => {
         <TableBody>
           <TableRow>
             <TableCell>App Name</TableCell>
-            <TableCell  >{props.appName || "None"}</TableCell>
+            <TableCell  >{props.appName || <DoNotDisturbIcon sx={{ color: "red" }} />}</TableCell>
           </TableRow>
           <TableRow>
             <TableCell>Node Name</TableCell>
-            <TableCell  >{props.nodeName || "None"}</TableCell>
+            <TableCell  >{props.nodeName || <DoNotDisturbIcon sx={{ color: "red" }} />}</TableCell>
           </TableRow>
           <TableRow>
             <TableCell>Pod Name</TableCell>
-            <TableCell  >{props.podName || "None"}</TableCell>
+            <TableCell  >{props.podName || <DoNotDisturbIcon sx={{ color: "red" }} />}</TableCell>
           </TableRow>
           <TableRow>
             <TableCell>Pod Namespace</TableCell>
-            <TableCell  >{props.podNamespace || "None"}</TableCell>
+            <TableCell  >{props.podNamespace || <DoNotDisturbIcon sx={{ color: "red" }} />}</TableCell>
           </TableRow>
           <TableRow>
             <TableCell>POD IP</TableCell>
-            <TableCell  >{props.podIp || "None"}</TableCell>
+            <TableCell  >{props.podIp || <DoNotDisturbIcon sx={{ color: "red" }} />}</TableCell>
           </TableRow>
           <TableRow>
             <TableCell>Env</TableCell>
-            <TableCell  >{props.env || "None"}</TableCell>
+            <TableCell  >{props.env || <DoNotDisturbIcon sx={{ color: "red" }} />}</TableCell>
           </TableRow>
         </TableBody>
       </Table>
     </TableContainer>
-
   );
 }
